58fa6052082740a5f24e3619e88511d6e7e32ac8
kiran <kiran2245012@gmail.com>
2025-04-01 20:43:12 +0530

    made changes

--- App.js
+++ App.js
@@ -6,24 +6,24 @@
 // child.innerHTML = "this is a child";
 // heading1.appendChild(child);
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// // root.render(heading);
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
 
-// const parent = React.createElement("div", { id: "parent" }, [
-//   React.createElement("div", { id: "child" }, [
-//     React.createElement("h1", { id: "siblings" }, "I'm first h1 tag"),
-//   ]),
-//   React.createElement("div", { id: "child2" }, [
-//     React.createElement("h1", { id: "siblings" }, "I'm second h1 tag"),
-//   ]),
-// ]);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "siblings" }, "I'm first h1 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "siblings" }, "I'm second h1 tag"),
+  ]),
+]);
 
-// root.render(parent);
+root.render(parent);
 
 // JSX
 
